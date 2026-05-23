541ace4d5deabc82dbcbe88a9dff14f83ea73a3a
anhthan17a2 <anhthan17a2@gmail.com>
2026-05-23 08:38:35 +0700

    update

--- .next/server/app/api/drawings/[id]/annotations/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/drawings/[id]/annotations/route"] = {"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
